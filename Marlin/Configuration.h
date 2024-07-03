0deb790bc18c62fa1ee70d7f996cb1955714b190
Jorge Verdugo <jorgeverdugoch@gmail.com>
2024-07-02 21:44:50 -0400

    feat: Filament runout sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1294,7 +1294,7 @@
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  * By default the firmware assumes HIGH=FILAMENT PRESENT.
  */
-//#define FILAMENT_RUNOUT_SENSOR
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
